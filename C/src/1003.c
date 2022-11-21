2568c7dee7055c75eecd4f3fc0a04a2feb931506
abraao 284 <abraao-284@outlook.com>
2022-11-21 10:49:14 -0300

    Simple sum

---
+++ C/src/1003.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    int x,y;
+    scanf("%i\n%i",&x,&y);
+    printf("SOMA = %i\n",(x+y));
+    return 0;
+}
---
+++ csharp/1003/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+        Console.WriteLine("SOMA = " + (x + y));
+    }
+}
