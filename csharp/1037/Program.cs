b7298b016ba111c6a08d44c7c103ab21291a6072
abraao 284 <abraao-284@outlook.com>
2022-11-28 09:36:19 -0300

    Interval

---
+++ csharp/1037/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double x = double.Parse(Console.ReadLine());
+        string interval;
+
+        if (x >= 0 && x <= 25)
+            interval = "Intervalo [0,25]";
+        else if (x > 25 && x <= 50)
+            interval = "Intervalo (25,50]";
+        else if (x > 50 && x <= 75)
+            interval = "Intervalo (50,75]";
+        else if (x > 75 && x <= 100)
+            interval = "Intervalo (75,100]";
+        else
+            interval = "Fora de intervalo";
+        Console.WriteLine(interval);
+    }
+}
--- python/1035.py
+++ python/1035.py
@@ -1,9 +1,6 @@
 a, b, c, d = input().split(" ")
 a, b, c, d = int(a), int(b), int(c), int(d)
 valor_aceito = "Valores nao aceitos"
-if d > a and b > c:
-    if a+b < c+d:
-        if (c > 0 and d > 0):
-            if a % 2 == 0:
-                valor_aceito = "Valores aceitos"
+if d > a and b > c and a % 2 == 0 and c > 0 and d > 0 and a+b < c+d:
+    valor_aceito = "Valores aceitos"
 print(valor_aceito)
